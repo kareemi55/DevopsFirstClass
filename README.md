7686fd6fe3b698e256b63c07f34029fe49f429c5
kareemi55 <kareemabid55@gmail.com>
2023-08-21 18:12:18 +0500

    addinng files

---
+++ README.md
@@ -0,0 +1,7 @@
+Steps to execture this script 
+
+   -> do npm install
+   -> do node server.js
+   -> check http://localhost:5000
+   -> we have route name http://localhost:5000/data
+   
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.get('/data', (req, res) => {
+    res.send('hello this is learning class')
+})
+
+app.listen(port, (err, data) => {
+    if(err) {
+        console.log(err)
+    }else {
+        console.log(`http://localhost:${port}`)
+    }
+})
